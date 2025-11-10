d3a88667fb7b0680158f76c1d37ae860d8403013
Lucas Chiappinigit <chiappinilucas@gmail.com>
2025-11-10 15:11:07 -0300

    Cascara unit tests de Bot core

---
+++ test/Library.Tests/Tests/BotCoreTest.cs
@@ -0,0 +1,35 @@
+using Library.Fachadas;
+
+namespace Library.Tests;
+
+public class BotCoreTest
+{
+
+    [Test]
+    public void MostrarListaComando()
+    {
+        //logica
+    }
+    
+    [Test]
+    public void BotRespondeSaludo ()
+    {
+      //logica
+    }
+    
+    [Test]
+    public void BotEnviaMensaje ()
+    {
+        //logica
+    }
+    
+    [Test]
+    public void FinalizarConversacion()
+    {
+        //logica
+    }
+    
+    
+
+    
+}
