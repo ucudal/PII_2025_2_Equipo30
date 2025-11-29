97a38fc4c4ddb44a168b9126c80b7f5f5ca4ec00
aristov983 <earis4499@gmail.com>
2025-11-29 19:06:02 -0300

    Documentadas varias clases.

--- src/Library/Clases principales/PanelResumen.cs
+++ src/Library/Clases principales/PanelResumen.cs
@@ -1,10 +1,21 @@
 using Library.Clases_tipos;
 
 namespace Library.Clases_principales;
-
+/// <summary>
+/// Clase para integrar en comando de panel resumen.
+/// </summary>
 public class PanelResumen
 {
+    /// <summary>
+    /// Representa el número total de clientes.
+    /// </summary>
     public int TotalClientes { get; set; }
+    /// <summary>
+    /// Representa las interacciones recientes.
+    /// </summary>
     public List<string> InteraccionesRecientes { get; set; } = new List<string>();
+    /// <summary>
+    /// Representa las reuniones proximas. 
+    /// </summary>
     public List<Reunion> ReunionesProximas { get; set; } = new List<Reunion>();
 }
